62271b0bff7b15841bc78c7a51514f07bd8a8ceb
diautzi <dia_utzi@yahoo.com>
2019-04-21 10:50:30 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      # Create a new route called /cart to show the items in your cart
       if @@cart.empty?
        resp.write "Your cart is empty"
       else
@@ -23,13 +24,17 @@ class Application
        end
      end
    elsif req.path.match(/add/)
+     # Create a new route called /add that takes in a GET param with the key item
      item_to_add = req.params["item"]
+     # can be solved like this, having an if/else loop inside the elif, using ewsp.write to print the output
      # if @@items.include? item_to_add
      #   @@cart << item_to_add
      #   resp.write "added #{item_to_add}"
      # else
      #   resp.write "We don't have that item!"
      # end
+
+     # or using the helper method, look at the bottom!
      resp.write handle_add(item_to_add)
    else
       resp.write "Path Not Found"
@@ -37,6 +42,7 @@ class Application
 
     resp.finish
   end
+
 
   def handle_search(search_term)
     if @@items.include?(search_term)
@@ -47,12 +53,12 @@ class Application
   end
 
   def handle_add(item_to_add)
-    if items.include? item_to_add
+    # helper method for the add route that takes in a GET param with key["item"]
+    if @@items.include? item_to_add
       @@cart << item_to_add
-      resp.write "added #{item_to_add}
+       return "added #{item_to_add}"
     else
-      resp.write "We dont't have that item!"
+     return "We don't have that item!"
     end
   end
-
 end
